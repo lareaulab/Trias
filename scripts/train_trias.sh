caaa444a1a3839542018c0a69578c1159408ed89
marjanfaizi <marjanfaizi@web.de>
2025-05-13 14:54:54 -0700

    Create train_trias.sh

---
+++ scripts/train_trias.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+WANDB_PROJECT=Trias python3 ../src/trias/main.py \
+    --report_to wandb \
+    --fp16 True \
+    --gradient_checkpointing False \
+    --seed 42 \
+    --run_name trias \
+    --max_seq_len 2 ... \
+    --vocab_file ... \
+    --output_dir ... \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8  \
+    --learning_rate 1e-4 \
+    --train_len 9956303 \
+    --weight_decay 0.01 \
+    --gradient_accumulation_steps 4 \
+    --max_steps 650000 \
+    --logging_steps 10000 \
+    --evaluation_strategy steps \
+    --eval_steps 50000 \
+    --save_strategy steps \
+    --save_steps 50000 \
+    --save_total_limit 3 \
