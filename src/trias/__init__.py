2cd64b9f1d63c5a5081ef3888c41972cc45fbfb5
marjanfaizi <marjanfaizi@web.de>
2025-05-13 20:45:11 -0700

    Update __init__.py

--- src/trias/__init__.py
+++ src/trias/__init__.py
@@ -1,1 +1,7 @@
+from transformers import AutoConfig, AutoTokenizer
+from .tokenizer import TriasTokenizer
+from .configuration import TriasConfig
 
+
+AutoConfig.register("trias", TriasConfig)
+AutoTokenizer.register(TriasConfig, TriasTokenizer)
