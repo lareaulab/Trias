0ebf0a85f472a39d5704443aeb68ebde947a509e
marjanfaizi <marjanfaizi@web.de>
2025-06-26 18:23:04 -0700

    Update notebook_utils.py

--- notebooks/notebook_utils.py
+++ notebooks/notebook_utils.py
@@ -13,6 +13,27 @@ import seaborn as sns
 
 
 
+# Dictionary mapping codons and amino acids
+codon_to_aa = {
+    "TTT": "Phe", "TTC": "Phe", "TTA": "Leu", "TTG": "Leu",
+    "TCT": "Ser", "TCC": "Ser", "TCA": "Ser", "TCG": "Ser",
+    "TAT": "Tyr", "TAC": "Tyr", "TAA": "Stop", "TAG": "Stop",
+    "TGT": "Cys", "TGC": "Cys", "TGA": "Stop", "TGG": "Trp",
+    "CTT": "Leu", "CTC": "Leu", "CTA": "Leu", "CTG": "Leu",
+    "CCT": "Pro", "CCC": "Pro", "CCA": "Pro", "CCG": "Pro",
+    "CAT": "His", "CAC": "His", "CAA": "Gln", "CAG": "Gln",
+    "CGT": "Arg", "CGC": "Arg", "CGA": "Arg", "CGG": "Arg",
+    "ATT": "Ile", "ATC": "Ile", "ATA": "Ile", "ATG": "Met",
+    "ACT": "Thr", "ACC": "Thr", "ACA": "Thr", "ACG": "Thr",
+    "AAT": "Asn", "AAC": "Asn", "AAA": "Lys", "AAG": "Lys",
+    "AGT": "Ser", "AGC": "Ser", "AGA": "Arg", "AGG": "Arg",
+    "GTT": "Val", "GTC": "Val", "GTA": "Val", "GTG": "Val",
+    "GCT": "Ala", "GCC": "Ala", "GCA": "Ala", "GCG": "Ala",
+    "GAT": "Asp", "GAC": "Asp", "GAA": "Glu", "GAG": "Glu",
+    "GGT": "Gly", "GGC": "Gly", "GGA": "Gly", "GGG": "Gly",
+}
+
+
 def generate_prompt_from_dataset(dataset):
     prompt = f">>{dataset['species_name']}<< {dataset['protein']}"
     return prompt
